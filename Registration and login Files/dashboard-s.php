2d23b9069dc358b3a272f48c707a4133e8a2be37
divyesh-gehlot <gehlotdivyeshsingh@gmail.com>
2024-11-10 22:12:21 +0530

    Student Dashboard in Progress

--- Registration and login Files/dashboard-s.php
+++ Registration and login Files/dashboard-s.php
@@ -1,5 +1,5 @@
 <?php
-echo "<script>alert('LOGIN SUCCESSFUL!');</script>";
+// echo "<script>alert('LOGIN SUCCESSFUL!');</script>";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,8 +24,18 @@ echo "<script>alert('LOGIN SUCCESSFUL!');</script>";
     </div>
 
     <div class="dashboard-container">
-        <h2>Welcome STUDENT!</h2>
-        <h3>Your Subjects and Marks</h3>
+        <h2 class="dashboard-s-h2">Welcome STUDENT!</h2>
+
+    <form action="" method="POST" class="form">
+            <label for="username">Enter your Roll Number:</label>
+            <input type="text" id="rollnumber" name="rollNumber" placeholder="Enter your roll number" required>
+
+            <div class="button-student">
+                <button type="submit" class="register" name="submit">See Marks</button>
+            </div>
+        </form>
+
+        <h3 class="dashboard-s-h3">Your Subjects and Marks</h3>
 
         <!-- Subjects and Marks Table -->
         <table class="marks-table">
@@ -34,9 +44,20 @@ echo "<script>alert('LOGIN SUCCESSFUL!');</script>";
                     <th>Marks</th>
                     <th>Feedback</th>
                 </tr>
-                <tr>
-                    <td>Mathematics</td>
-                    <td>85</td>
+                <?php
+                $roll=$_POST["rollNumber"]
+                require 'config.php';
+                $query=sql_query($conn , "select roll_number,subject,marks from student_marks where roll_number = $roll");
+                ?>
+
+
+
+
+
+
+
+
+                <!-- <tr>
                     <td>
                         <form action="submit_feedback.php" method="POST" class="feedback-form">
                             <input type="hidden" name="subject" value="Mathematics">
@@ -44,7 +65,7 @@ echo "<script>alert('LOGIN SUCCESSFUL!');</script>";
                             <button type="submit" class="feedback-button">Submit</button>
                         </form>
                     </td>
-                </tr>
+                </tr> -->
         </table>
     </div>
 
